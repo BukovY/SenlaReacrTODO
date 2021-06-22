618c267a4c863bad9e33a0e305c4a9d0be00c9da
BukovY <zoroo@mail.ru>
2021-06-22 20:37:21 +0300

    add disabled button

--- todo/src/component/AddTask.js
+++ todo/src/component/AddTask.js
@@ -4,7 +4,7 @@ const AddTask = ({ newTask, taskHandler, addTask}) => {
     return (
         <div>
             <textarea placeholder='Введите задачу' value={newTask} onChange={(event) => taskHandler(event.target.value)}/>
-            <button id='done' onClick={() => addTask()}>Done</button>
+            <button id='done' onClick={() => addTask()} disabled={!newTask && 'true'}>Done</button>
         </div>
     );
 };
