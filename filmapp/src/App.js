892d09a1b2b52cdacd3e5598c4889ce1c0f52d6d
Быков Евгений <76494975+BukovY@users.noreply.github.com>
2021-07-20 09:28:18 +0300

    delete comment

--- filmapp/src/App.js
+++ filmapp/src/App.js
@@ -55,7 +55,6 @@ export default class App extends Component {
                     }
                 }
             },
-            // add-change film
             title: {
                 value: '',
                 valid: true,
--- my-ap/src/redux/store.js
+++ my-ap/src/redux/store.js
@@ -8,17 +8,3 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 export default store
-/*
-const rootReducer = combineReducers({
-    todos: todosReducer
-})
-
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-    rootReducer,
-    //compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-)
-
-export default store
- */
